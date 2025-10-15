22aeac07873407a973f828b49af54703b86f3f74
Milosz <m.szydlowski2003@gmail.com>
2025-10-15 19:51:49 +0200

    draw dot finished

--- src/app.rs
+++ src/app.rs
@@ -1,3 +1,5 @@
+use std::{thread, time::Duration};
+
 use minifb::{MouseButton, MouseMode, Window, WindowOptions};
 
 use crate::{canvas::Canvas, common::XY, shapes::Shape, ui::UIElement};
@@ -53,8 +55,8 @@ impl App {
             // let mouse_pos_usize: (usize, usize) = (mouse_pos.0 as usize, mouse_pos.1 as usize);
             // self.canvas[mouse_pos_usize] = brush_color;
 
-            let (x, y): (usize, usize) = (mouse_pos.0 as usize, mouse_pos.1 as usize);
-            draw_dot(&mut self.canvas, XY { x, y }, 4 as usize, brush_color);
+            let (x, y) = (mouse_pos.0 as usize, mouse_pos.1 as usize);
+            draw_dot(&mut self.canvas, XY { x, y }, 3, brush_color);
         }
     }
 
@@ -69,45 +71,27 @@ impl App {
     }
 }
 
-pub fn draw_dot<T, U>(canvas: &mut Canvas, point: XY<T>, radius: U, color: u32)
-where
-    T: Into<usize> + Copy,
-    U: Into<usize> + Copy,
-{
-    let point: XY<usize> = XY {
-        x: point.x.into(),
-        y: point.y.into(),
+pub fn draw_dot(canvas: &mut Canvas, point: XY<usize>, radius: usize, color: u32) {
+    let point = XY {
+        x: point.x as i32,
+        y: point.y as i32,
     };
-    let radius: usize = radius.into();
+    let radius = radius as i32;
 
-    let x_distance = (2 * radius) - radius.saturating_sub(point.x);
-    let y_distance = (2 * radius) - radius.saturating_sub(point.y);
-
-    let start_x = point.x.saturating_sub(radius);
-    let start_y = point.y.saturating_sub(radius);
-
-    for x in 0..x_distance {
-        for y in 0..y_distance {
-            // let distance = (((point.x + x).pow(2) + (point.y + y).pow(2)) as f32).sqrt();
-            // if distance < radius as f32 {
-            canvas[(x + start_x, y + start_y)] = color;
-            // }
+    for x in -radius..=radius {
+        for y in -radius..=radius {
+            let distance = ((x.pow(2) + y.pow(2)) as f32).sqrt();
+            if distance <= radius as f32 {
+                let x = point.x - x;
+                let y = point.y - y;
+                if x > 0 && y > 0 {
+                    canvas[(x as usize, y as usize)] = color;
+                }
+            }
         }
     }
 }
 
-pub fn draw_line<T>(canvas: &mut Canvas, p1: XY<T>, p2: XY<T>)
-where
-    T: Into<usize> + Copy,
-{
-    let p1 = XY {
-        x: p1.x.into(),
-        y: p1.y.into(),
-    };
-    let p2 = XY {
-        x: p2.x.into(),
-        y: p2.y.into(),
-    };
-
+pub fn draw_line(canvas: &mut Canvas, p1: XY<i32>, p2: XY<i32>) {
     todo!()
 }
